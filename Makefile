4aef0d5d7f31129e6775727df5aee3732eb11fc7
Ben Eggers <beggerss@cs.washington.edu>
2014-05-06 14:55:30 -0700

    started on stats file

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 FLAGS=-Wall -O3 -ffast-math -funroll-loops
 
-all:	test
+all: test stats
 
 Cover_Tree_Point.o: Cover_Tree_Point.h Cover_Tree_Point.cc
 	g++ -c $(FLAGS) Cover_Tree_Point.cc
 
 test: test.cc Cover_Tree.h Cover_Tree_Point.o
 	g++ $(FLAGS) -o test test.cc Cover_Tree_Point.o
+
+stats: statistics.cc Cover_Tree.h Cover_Tree_Point.o
+	g++ $(FLAGS) -o statistics statistics.cc Cover_Tree_Point.o
 
 clean:
 	rm -f *.o test
---
+++ statistics.cc
@@ -0,0 +1,34 @@
+// Author: Ben Eggers (beggerss@cs.washington.edu)
+// GNU GPL'd
+
+// This code generates a cover tree from the dataset passed in as a command-line
+// parameter, and gathers statistics about the structure of the tree. Or at least,
+// that's what it WILL do...
+
+#include "./Cover_Tree.h"
+#include "./Cover_Tree_Point.h"
+
+// Reads in the file, and creates Cover_Tree_Points from its contents. The file 
+// must be formatted as follows:
+//  - Lines beginning with a "#" will be ignored
+//  - One point per line
+//  - Points line in n-dimensional euclidean space
+//  - Lines must be numbers (floating-point or integer) delimited by any non-number
+//    and non-decimal characters
+//  - The nth number of each line defines that point's position on the nth axis
+//  - If some lines in the same file have more numbers than others, all others 
+//    will be padded with 0's so that each point lies in the same-dimensional space
+//
+// Returns a vector<Cover_Tree_Point> representing all the points in the file.
+vector<Cover_Tree_Point> readFile(FILE *f);
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		printf("Put in a data file! Noob.\n");
+		exit(1);
+	}
+}
+
+vector<Cover_Tree_Point> readFile(FILE *f) {
+
+}
