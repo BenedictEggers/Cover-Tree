dd68f2371b1b7d24bb7774c4312ff5ab07e46d04
Ben Eggers <beggerss@cs.washington.edu>
2014-06-06 11:54:06 -0700

    did recursive part of make_ball

--- gen_data.py
+++ gen_data.py
@@ -6,16 +6,35 @@
 # their data in sub-balls and (sub-)*balls. It also generates some test queries,
 # and writes everything out to a file in "./test_data/".
 
+import random
+import math
+
+# These are needed by all the recursive calls (globals suck, I know, but it's nice
+# to be able to modify program behavior by just messing with things at the top).
+
+# How much we should scale the radius of the balls when we recurse to sub-balls
+RADIUS_SCALE = .25
+# How many sub-ball recursions we should try. Note that there will likely be fewer
+# than this many (see make_ball for the reason).
+NUM_SUBBALLS = 10
+
 def main():
 	# These parameters can be used to tweak the program behavior. "levels" is how
 	# many levels down the ball-generating will recurse, and "center", "radius"
-	# together define the circle within which all the points will fall.
+	# together define the circle near which all the points will fall. Note that
+	# some points may fall outside of the circle, but they will be near. "k" is
+	# the number of nearest-neighbors we want for each point. "count" is how
+	# many test queries we want
 	center = (0, 0)
 	radius = 10
 	levels = 1
+	k = 1
+	count = 10
 
 	points = make_ball(center, radius, levels)
-	queries = get_queries(points)
+	queries = get_queries(points, count, k, radius)
+
+	write_to_file(points, queries)
 
 
 def make_ball(center, radius, level):
@@ -28,21 +47,41 @@ def make_ball(center, radius, level):
 	"""
 	if level is 1:
 		# we bottomed out the recursion, let's get some points
+		
 
 	else:
-		# need to recurse
+		# We need to recurse. We're going to make up to 10 balls of radius r/4,
+		# placing them randomly within our ball.
+		sub_balls = []
+		for x in range(NUM_SUBBALLS):
+			# It'd be nice to get these all in the circle, but whatever
+			new_ball = (random.randint(-radius, radius), random.randint(-radius, radius))
+			for old_ball in sub_balls:
+				# make sure there are no conflicts
+				break if distance(old_ball, new_ball) < radius / RADIUS_SCALE:
+			else:
+				sub_balls.append(new_ball)
 
+		return [make_ball(b, radius/RADIUS_SCALE, level-1) for b in sub_balls]
 
-def get_queries(points, bound=1000):
+def get_queries(points, count, k, bound=1000):
 	"""
 	Takes in a list of points, and makes some test queries on it. The optional
 	bound parameter is to tell the function within what range the query points
-	should lie in: ([-bound, bound], [-bound, bound]). Returns a list of
-	(query, k, answer_list) tuples where query is some (x, y) query point,
-	k is the length of answer_list, and answer_list is the k nearest points
-	to query, maybe more if multiple points are tied for last place.
+	should lie in: ([-bound, bound], [-bound, bound]). Returns a list of length
+	"count" (query, answer_list) tuples where query is some (x, y) query point
+	and answer_list is the k nearest points to query, maybe more if
+	multiple points are tied for last place.
 	"""
+	
+	queries = []
 
+	for x in range(count):
+		query = (random.randint(-count, count), random.randint(-count, count))
+
+
+def distance(ball1, ball2):
+	return math.sqrt((ball2[0] - ball1[0])**2 + (ball2[1] - ball1[1])**2)
 
 if __name__ == "__main__":
 	main()
