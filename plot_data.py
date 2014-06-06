416a67f39de0ab2a1ca1e2f66cd8b8131c688aa9
Ben Eggers <beggerss@cs.washington.edu>
2014-06-06 13:23:12 -0700

    started writing data plotter

---
+++ plot_data.py
@@ -0,0 +1,7 @@
+# Ben Eggers <ben.eggers36@gmail.com>
+# GNU GPL'd
+
+# This script takes in a file name (should be in test_data) with NO EXTENSION. It
+# will plot the points found in filename.point, then the queries found in
+# filename.query in a different color, and the answers to the knn queries for that
+# query in a third color. See gen_data.py for more detail on the data format.
